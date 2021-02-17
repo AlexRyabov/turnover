60069df4f6759141f2bf4620101d6cb36c387363
Alexey Ryabov <32523192+AlexRyabov@users.noreply.github.com>
2021-02-17 10:56:03 +0100

    Add files via upload

--- turnover.R
+++ turnover.R
@@ -6,61 +6,84 @@
 #' 
 #' @description
 #' 
-#' SER = turnover_s(X, method = "SERa")
-#' returns a matrix SER(i, j) with  turnover indexes between the rows i and j of the data in X
 #' 
-#' 
-#' lSER = turnover(X, method = "SERa", dates = NULL, locations = NULL)
-#' Return a list lSER of M x M matrices characterizing species turnover between observation i and j of X 
-#' measured at given dates and locations. 
+#' lSER = turnover(X, method = "SERr",  combinations = "i<j", 
+#'               dates = NULL, locations = NULL, measure = "euclidean", 
+#'               ext_inv = FALSE, groupby = NULL)
+#' This function returns a data frame lSER of the turnover between rows i and j 
+#' of matrix X. lSER$From = i, lSER$To = j, lSER$SER is the turnover between these rows. 
 #' 
 #' The function result contains:
-#' lSER$SER is an upper triangle M x M matrix with turnover index. lSER$SER[i,j] turnover between row i and j in X
-#' 
-#' lSER$S_common, lSER$S_ext, lSER$S_imm, lSER$S_total are an upper triangle MxM matrices  
-#' the effective number of common/extinct/immigrating/total species,
-#' e.g., lSER$S_total[i,j] is the total number of species in observation i and j
-#' and  lSER$S_ext[i,j] the number of species observed in i but not in j
+#' lSER$From is the row number of 'from' observation in X
+#' lSER$To is the row number of 'to' observation
+#' lSER$SER is the turnover between these observations
+#' if ext_inv = TRUE, then the result includes additionally 
+#' the effective number of common/extinct/immigrating/total species for 
+#' observation i and j, lSER$S_common, lSER$S_ext, lSER$S_imm, lSER$S_total 
+#' e.g., lSER$S_total is the total number of species in observation i and j
+#' and  lSER$S_ext the number of species observed in i but not in j
 #' Note that the effective species number is richness based when  method="SERr" and 
 #' abundance (Simpson index) based when method="SERa", 
-#' see details in Hillebrand, H. et al. J Appl Ecol 55, 169-184 (2018).
 #' 
-#' lSER$TimeIntv[i,j] time intervals between observation i and j
+#' if dates are specified, lSER$TimeIntv is the time interval between observation i and j, 
 #' 
-#' lSER$Dist[i,j]  spatial distances between observation i and j
-#' 
-#' 
-#' lSER_gr = turnover_g(X, method = "SERa", dates = NULL, locations = NULL, groupby)
-#' This function calculates turnover within groups defined by groupby dataframe. 
-#' The groupby must contain the same number of rows as X. The function splits X 
-#' into groups and returns a sheet with two fields: lSER_gr$groupnames contains 
-#' unique group names, lSER_gr$turnover contains information on species turnover 
-#' within each group. 
-#' 
-#' 
-#' 
-#' 
-#' 
+#' if locations are specified, lSER$Dist is the spatial distances between observation i and j, 
 
-#' @param X  is  (M x N) table or matrix with M observations of N species. 
+#' lSER = turnover(X, method = "SERa", dates = NULL, locations = NULL, measure = "euclidean", groupby)
+#' calculates turnover within groups defined by groupby dataframe. 
+#' groupby must contain the same number of rows as X. Function turnover_g(X, ...) splits X 
+#' into groups and returns a data frame with the same structure as returned by  function turnover(X, ...), 
+#' plus an additional field "groupname"   
+#' 
+#' 
+#' 
+#' 
+#' @param X  is  (M x N) data frame or matrix with M observations of N species. 
 #'   X can contain species abundances, frequencies or, only for SERr, presence/absence data 
 #'   if X contains abundance then for calculating SERa the abundances will be normalized, 
 #'   so that the sum in each row equal 1. 
-#' @param method  equals  "SERr" for calculating richness based turnover index 
-#' SER_ij  = (S_immigrant + S_extinct)/S_total, effective number of species is richness based
+#' @param method  must be one of "SERr" and  "SERa". 
+#' If method = "SERr", then richness based turnover index is calculated 
+#' SER_ij  = (S_immigrant + S_extinct)/S_total, 
+#' and the effective numbers of species are richness based
 #' 
-#' @param method  equals  "SERa" for calculating abundance based index 
+#' If method = "SERa", then  abundance based turnover index is  calculated
 #'  SER_ij = (sum_k pik^2 + sum_k pjk^2 - 2  sum_k( pik  pjk) ) / (sum_k pik^2 + sum_k pjk^2 -  sum_k( pik * pjk) )
-#' 
+#' and the effective numbers of species are based on Simpson index
+#'  
+#' @param combinations	indicates for which combinations of 
+#' rows i and j of matrix X the turnover must be calculated. The available 
+#' values are "i<j", "i!=j" and "i,j". 
+#' For combinations="i<j", the turnover is calculated between each row i and all subsequent 
+#' rows j of matrix X. This type is useful for calculating temporal turnover, 
+#' because in this case (if the data are sorted by date) we will calculate 
+#' turnover from past to future, but not vice versa.  
+#' For combinations="i!=j", turnover is calculated for all combinations of i and j, 
+#' except for i=j. 
+#' For combinations="i,j", the turnover is computed for all combinations 
+#' of i and j, in which case the turnover for i=j is zero. This option is useful 
+#' if the results should be converted later into a square matrix T, such that 
+#' element T[i,j] shows the turnover between rows X[i, :] and X[j, :].
 #'  
 #' @param dates observation dates (datetime) or any numbers e.g. observation years, 
 #' 
 #' @param locations spatial coordinates, typically should be Mx1, for 1D gradients, 
 #' Mx2 matrix if 2 coordinates are know, or Mx3 matrix for a 3D habitats
+#' or  longitude and latitude (in this order!) in this case measure = "lonlat"
+#' 
+#' @param measure measure for measuring distance can be any of measures used 
+#' by dist(X, ..) function, or can be "lonlat" in this case the geodesic distance 
+#' in meters is measured  using 'geodist' package. 
+#' This package should be installed before. the 1st column of locations should 
+#' contain longitude and the second column latitude
+#' 
+#' @param ext_inv should the information about invaded, extinct, common and total 
+#' species number be included?
 #'  
-#' @param groupby (M x ..) a 'factor' in the sense that as.factor(f) defines 
-#' the grouping, or a list of such factors in which case their interaction 
-#' is used for the grouping.  (see split(x, ...) function)
+#' @param groupby  is a column or data frame with M rows  which are used to  
+#'  group the data  (see split(x, ...) function). In this case turnover is 
+#'  calculated within groups defined by "groupby" and resutl contains column 
+#'  groupname.
 #'  
 #'  
 
@@ -70,24 +93,19 @@
 #' SER = turnover_s(X, method = "SERr")
 #' 
 #' lSER = turnover_s(X, method = "SERa", dates)
-#' lSER = turnover_s(X, method = "SERa", NULL, location)
+#' spatial turnover, when coordinates are longitude and lattitude 
+#' lSER = turnover_s(X, method = "SERa", location = LonLat, measure = "lonlat")
 #' 
 #' Calculating temporal species turnover separately for multiple 
 #' stations 
-#' lSER_gr = turnover_g(X, method = "SERa", ObservationDates, locations = NULL, StationList)
+#' lSER_gr = turnover_g(X, method = "SERa", dates = ObservationDates, groupby = StationList)
 #' where StationList is a data frame with station names for each observation. 
-#' Output: lSER_gr$groupnames contains a list of unique station names, 
-#' lSER_gr$turnover is a list of turnover information for those stations. 
-#' The elements of this list have a structure returned by the function 
-#' lSER = turnover(X, ...)
-#' For example, the name of the second group 
-#' lSERa_gr$groupnames[2] 
-#' Plot temporal turnover for the second group 
-#' plot(lSERa_gr$turnover[[2]]$TimeIntv, lSERa_gr$turnover[[2]]$SER)
+#' Output: lSER_gr$groupname contains station names, 
+#' lSER_gr$SER is turnover information calculated within each station, but not between them. 
 #' 
 
 
-turnover_s <- function (X, method = "SERr") {
+turnover_s <- function (X, method = "SERr", combinations = "i<j") {
   #short version, returns only turnover index
   #between rows of the X 
   
@@ -100,94 +118,129 @@ turnover_s <- function (X, method = "SERr") {
 
   #method = 'SERr' output is based on presence/absence data
   #SER_ij  = (S_immigrant + S_extinct)/S_total
-
-  #the output SER(,) is  MxM matrix with turnover rates between different pairs of observations. 
-  #SER(i,j) is turnover from observation i to j
-
-  Res = turnover(X, method, NULL)
-  return(Res$SER)
-}
-
-
-turnover_g <- function(X, method = "SERr", dates = NULL, locations = NULL, groupby = GroupBy){
-  Xsplit = split(X, groupby);
-  if (!is.null(dates))     { DatesSplit = split(dates,     groupby)}
-  DatesPiece = NULL;
-  if (!is.null(locations)) { LocSplit   = split(locations, groupby)}
-  LocPiece = NULL;  
-  Res <- vector(mode = "list", length = length(Xsplit))
-  for(i in 1:length(Xsplit)){
-    if (!is.null(dates))      { DatesPiece = DatesSplit[[i]]}
-    if (!is.null(locations))  { LocPiece =   LocSplit[[i]]}
-    
-     Res[[i]] =  turnover(Xsplit[[i]], method, DatesPiece, LocPiece)
-  }
-  return(list(groupnames = names(Xsplit), 'turnover' = Res))
-}
-
-
-turnover <- function (X, method = "SERr", dates = NULL, locations = NULL) {
-  switch(method, 
-         SERa={
-           # abundance based turnover...
-           Res = turnover_a(X, method);
-         },
-         SERr={
-           # richness based turnover...
-           Res = turnover_r(X, method);
-         },
-         {
-           stop('turnover method is undefined')
-         }
-  )  
-
-  if (!is.null(dates)){
-    #get number of records 
-    M <- nrow(X);
-    
-    M2 = length(dates)
-    if (M != M2){
-      stop("The length of the dates array must be equal to the number of rows in X")
-    }
-    
-    #find differences in dates (if not null) or interval between columns
-    D = rep(dates, M);
-    D1 = matrix(D, M, M, byrow = FALSE);
-    D2 = matrix(D, M, M, byrow = TRUE);
-    
-    #D1 = as.Date(D1, origin ="1970-01-01")  unwrap D1 and D2
-    #D2 = as.Date(D2, origin ="1970-01-01")
-    #TimeIntv = difftime(D2, D1, units);
-    #need to pack to back into a matrix
-    #TimeIntv = matrix(TimeIntv, M, M, byrow = FALSE)
-    
-    TimeIntv = D2 - D1; #interval in days or integer numbers
-    #set elements below the main diagonal to zero
-    for(i in 2:M){
-      for (j  in 1:(i-1)){
-        TimeIntv[i,j] = NA;
-      }
-    }
-    Res$TimeIntv=TimeIntv;
-  }
   
-  if (!is.null(locations)){
-    M <- nrow(X);
-    M2 = nrow(locations)
-    if (M != M2){
-      stop("The number of rows in the location matrix must be equal to the number of rows in X.")
-    }    
-    Dist = as.matrix(dist(locations, method = "euclidean", diag = TRUE, upper = TRUE, p = 2))
-    for(i in 2:M){
-      for (j  in 1:(i-1)){
-        Dist[i,j] = NA;
-      }
-    }
-    Res$Dist=Dist;
-  }  
-  return(Res);
+  M = nrow(X); 
+  Res = turnover(X, method,  combinations)
+  return(Res)
 }
 
+
+
+
+turnover <- function (X, method = "SERr",  combinations = "i<j", 
+                      dates = NULL, locations = NULL, measure = "euclidean", 
+                      ext_inv = FALSE, groupby = NULL, RowID = NULL) {
+  M = nrow(X); 
+  if (!is.null(groupby))  {#if groups, then split and call turnover for each group
+    RowID = c(1:M); #assign row ID
+    RowIDSplit = split(RowID, groupby);
+    Xsplit = split(X, groupby);
+    if (!is.null(dates))     { DatesSplit = split(dates,     groupby)}
+    DatesPiece = NULL;
+    if (!is.null(locations)) { LocSplit   = split(locations, groupby)}
+    LocPiece = NULL;  
+    Res = NULL;
+    GroupNames = names(Xsplit)
+    for(i in 1:length(Xsplit)){
+      if (!is.null(dates))      { DatesPiece = DatesSplit[[i]]}
+      if (!is.null(locations))  { LocPiece =   LocSplit[[i]]}
+      #get turnover
+      TO =  turnover(Xsplit[[i]], method, combinations, DatesPiece, LocPiece, measure, ext_inv, NULL, RowIDSplit[[i]]);
+      #group name
+      TO$groupname = rep(GroupNames[[i]], nrow(TO));
+      #rbind results
+      if (is.null(Res)) {
+        Res = TO;
+      }else {
+        Res <- rbind(Res, TO)
+      }
+    }
+    return(Res)  
+  }else {#if no groups, then calculate turnover
+    switch(method, 
+           SERa={
+             # abundance based turnover...
+             Res = turnover_a(X, method);
+           },
+           SERr={
+             # richness based turnover...
+             Res = turnover_r(X, method);
+           },
+           {
+             stop('turnover method is undefined')
+           }
+    )  
+    
+    if(is.null(RowID)) { #assign rowID is not assigned before
+      RowID = c(1:M);
+    }
+    RowIDs = rep.int(RowID, M);
+    i_ind = matrix(RowIDs, nrow=M,byrow = FALSE);
+    j_ind = matrix(RowIDs, nrow=M,byrow = TRUE);
+
+  
+        
+    ord_ind = mat_index(Res$SER, combinations);
+    
+    From    = i_ind[ord_ind];
+    To      = j_ind[ord_ind];
+    SER     = Res$SER[ord_ind];
+    if (ext_inv){
+      S_total = Res$S_total[ord_ind];
+      S_common= Res$S_common[ord_ind];
+      S_imm   = Res$S_imm[ord_ind];
+      S_ext   = Res$S_ext[ord_ind];
+      Result = data.frame(From, To, SER, S_total, S_common, S_ext);
+    }
+    else {
+      Result = data.frame(From, To, SER);
+    }
+
+   if (!is.null(dates)){
+      #get number of records 
+      M <- nrow(X);
+      M2 = length(dates)
+      if (M != M2){
+        stop("The length of the dates array must be equal to the number of rows in X")
+      }
+      
+      #find differences in dates (if not null) or interval between columns
+      D = rep(dates, M);
+      D1 = matrix(D, M, M, byrow = FALSE);
+      D2 = matrix(D, M, M, byrow = TRUE);
+      
+      #D1 = as.Date(D1, origin ="1970-01-01")  unwrap D1 and D2
+      #D2 = as.Date(D2, origin ="1970-01-01")
+      #TimeIntv = difftime(D2, D1, units);
+      #need to pack to back into a matrix
+      #TimeIntv = matrix(TimeIntv, M, M, byrow = FALSE)
+      
+      TimeIntv = D2 - D1; #interval in days or integer numbers
+      #set elements below the main diagonal to zero
+      Result$TimeIntv = TimeIntv[ord_ind];
+    }
+    
+    if (!is.null(locations)){
+      M <- nrow(X);
+      M2 = nrow(locations)
+      if (M != M2){
+        stop("The number of rows in the location matrix must be equal to the number of rows in X.")
+      }    
+      if (tolower(measure)  == "lonlat") {
+        library("geodist");
+        Dist = geodist(locations, paired = TRUE, measure = "geodesic");
+        Result$Dist = Dist[ord_ind];
+      }else{
+        Dist = as.matrix(dist(locations, method = measure, diag = TRUE, upper = TRUE, p = 2))
+        Result$Dist = Dist[ord_ind];
+      }
+    }  
+    return(Result);
+  }
+}
+
+
+  
 
 turnover_a <- function(X, method) {
   X = as.matrix(X)
@@ -224,7 +277,7 @@ turnover_a <- function(X, method) {
   #SER2 = (P1_sq + P2_sq - 2 * P12)/(P1_sq + P2_sq - P12);
   
   Result = list(SER=SER, S_total = S_total, S_common = S_common, S_imm = S_imm, S_ext = S_ext);
-  Result = set2nullLowTriang(Result);
+ 
   return(Result)
 }
 
@@ -250,22 +303,52 @@ turnover_r <- function(X, method) {
   S_ext =  P1 %*% ((!P2) + 0);
   SER = (S_imm + S_ext)/(S_total);
   Result = list(SER=SER, S_total = S_total, S_common = S_common, S_imm = S_imm, S_ext = S_ext);
-  Result = set2nullLowTriang(Result);
   return(Result)
 }
 
-set2nullLowTriang <- function(Res){
-  #Set to NULL all elements below the main diagonal, because this elements correspond  
-  #to 'back-in-time' turnover rates
-  M <- nrow(Res$SER);
-  for(i in 2:M){
-    for (j  in 1:(i-1)){
-      Res$SER[i,j]      = NA;
-      Res$S_total[i,j]  = NA;
-      Res$S_common[i,j] = NA;
-      Res$S_imm[i,j]    = NA;
-      Res$S_ext[i,j]    = NA;
-    }
+# Mat2DataFrame <- function(Res, diag, upper, xnames){
+#   #Set to NULL all elements below the main diagonal, because this elements correspond  
+#   #to 'back-in-time' turnover rates
+#   M <- nrow(Res$SER);
+#   
+#   
+#   ij = rep.int(c(1:M), M);
+#   i_ind = matrix(ij, nrow=M,byrow = FALSE);
+#   j_ind = matrix(ij, nrow=M,byrow = TRUE);
+#   
+#   ind = mat_index(Res$SER, diag, upper);
+# 
+#   From    = i_ind[ind];
+#   To      = j_ind[ind];
+#   SER     = Res$SER[ind];
+#   S_total = Res$S_total[ind];
+#   S_common= Res$S_common[ind];
+#   S_imm   = Res$S_imm[ind];
+#   S_ext   = Res$S_ext[ind];
+#   
+#   return(data.frame(From, To, SER, S_total, S_common, S_ext));
+# }
+
+mat_index <- function(SampleMatrix, combinations){
+  if (!is.na(pmatch(combinations, "i<j"))) 
+    combinations <- "i<j"
+  COMBs <- c("i<j", "i!=j", "i,j")
+  combID <- pmatch(combinations, COMBs)
+  if (is.na(combID)) 
+    stop("invalid i,j combinations")
+  if (combID == -1) 
+    stop("ambiguous i,j combinations")
+  
+  indU = upper.tri(SampleMatrix, diag=FALSE);
+  if (combID == 1){ #upper part
+    ind = indU;
+  } else if (combID == 2) {  #upper and lower part
+    indL = lower.tri(SampleMatrix, diag=FALSE);
+    ind = indL | indU;
+  } else { #everything
+    indL = lower.tri(SampleMatrix, diag=TRUE);
+    ind = indL | indU;
   }
-  return (Res);
+          
+return(ind);
 }
--- turnover_examples.R
+++ turnover_examples.R
@@ -1,8 +1,7 @@
 #' Alexey Ryabov 2020
-#' examples of using turnover and turnover_s
+#' examples of using turnover, turnover_s and turnover_g
 
 source("turnover.R")
-
 ##read data
 ##
 data = read.csv( file = "Species.csv")
@@ -21,21 +20,23 @@ SERr = turnover_s(data[, SpecColumns]) #default parameters
 SERr = turnover_s(data[, SpecColumns], method = "SERr") #explicit
 #note that our richness based turnover index is equivalent to the binary distance in R
 #so you can get the same result using 
-SERr2 = as.matrix(dist(data[, SpecColumns][, ], method = "binary"))
+SERr2 = (dist(data[, SpecColumns][, ], upper = TRUE, diag = TRUE, method = "binary"))
+SERr2 = as.matrix(SERr2); #convert to a MxM matrix
+upper_ind = mat_index(SERr2, "i<j"); #select upper triangular part
 #both functions give the same result
-plot(SERr, SERr2)
+plot(SERr$SER, SERr2[upper_ind])
 
 #abundance based turnover index 
 SERa = turnover_s(data[, SpecColumns], method = "SERa")
 
 #plot SERr vs SERs
-plot(SERr, SERa)
+plot(SERr$SER, SERa$SER)
 
 #richness based turnover characteristics (list of turnover index + other metrics)
-lSERr = turnover(data[, SpecColumns], method = "SERr") 
+lSERr = turnover(data[, SpecColumns], method = "SERr", ext_inv = TRUE) 
 
 #abundance based turnover characteristics (list of turnover index + other metrics) 
-lSERa = turnover(data[, SpecColumns], method = "SERa")
+lSERa = turnover(data[, SpecColumns], method = "SERa", ext_inv = TRUE)
 
 #plot effective number of extinct species vs turnover index
 plot(lSERa$SER, lSERa$S_ext)
@@ -50,11 +51,18 @@ lSERa = turnover(data[, SpecColumns], method = "SERa", dates =  SampleDates)
 #plot turnover as a function time intervals
 plot(lSERa$TimeIntv/365, lSERa$SER)
 
-#plot turnover index as a function of distance between observations
+#plot turnover index as a function of euclidean distance between observations
 XY = data[, 2:3]; 
 lSERa = turnover(data[, SpecColumns], method = "SERa", locations =  XY)
 plot(lSERa$Dist, lSERa$SER)
 
+#plot turnover index as a function of euclidean distance between observations
+Longitude = c(1:nrow(data))/nrow(data);
+Latitude =  c(1:nrow(data))/nrow(data);
+LonLat = data.frame(Longitude, Latitude);
+
+lSERa = turnover(data[, SpecColumns], method = "SERa", locations =  LonLat, measure = "lonlat")
+plot(lSERa$Dist, lSERa$SER)
 
 #Group by some factors 
 #define stations and areas 
@@ -65,18 +73,16 @@ Station = sample(c("A", "B", "C"), size = nr, replace = TRUE)
 #
 StatArea = data.frame(Area, Station);
 SampleDates = as.Date(data$Date, format = "%Y-%m-%d"); #convert the input dates from string into class "date"
-lSERa_gr = turnover_g(data[, SpecColumns], method = "SERa", dates =  SampleDates, groupby = StatArea)
-
-#lSERa_gr is a list with the first element containing the names of groups and turnover information 
-#for this group
-#plot time interval SER for the second group
-GrID = 1;
-lSERa_gr$groupnames[GrID]
-plot(lSERa_gr$turnover[[GrID]]$TimeIntv, lSERa_gr$turnover[[GrID]]$SER)
+lSERa_SA = turnover(data[, SpecColumns], method = "SERa", dates =  SampleDates, groupby = StatArea)
+#lSERa_gr is a data frame with turnover within each group 
+#show group names
+summary(lSERa_SA)
+unique(lSERa_SA$groupname)
+plot(lSERa_SA$TimeIntv, lSERa_SA$SER)
 
 #group by area only
-lSERa_gr = turnover_g(data[, SpecColumns], method = "SERa", dates =  SampleDates, groupby = StatArea[, "Area"])
-GrID = 1;
-lSERa_gr$groupnames[GrID]
-plot(lSERa_gr$turnover[[GrID]]$TimeIntv, lSERa_gr$turnover[[GrID]]$SER)
+lSERa_A = turnover(data[, SpecColumns], method = "SERa", dates =  SampleDates, groupby = StatArea[, "Area"])
+summary(lSERa_A)
+unique(lSERa_A$groupname)
+plot(lSERa_A$TimeIntv, lSERa_A$SER)
 
